01626a021b5426c420d606c861c27201191cfc2d
Adryann Layon <adryann.layon7@gmail.com>
2023-03-09 13:39:59 -0300

    add: buttontext component

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  display: flex;
+  align-items: center;
+  text-align: left;
+  gap: 0.8rem;
+  background: none;
+  color: ${({ theme }) => theme.COLORS.PINK};
+  line-height: 2.1rem;
+  font-weight: 400;
+`
