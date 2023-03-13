f46459fe3c1e92aee224ac7ca6d2af5e3e9486ac
Adryann Layon <adryann.layon7@gmail.com>
2023-03-13 14:48:32 -0300

    add: SignIn component

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,24 @@
+import { Container, Form, Background } from './styles'
+import { FiMail, FiLock } from 'react-icons/fi'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assitir</p>
+
+        <h2>Faça seu login</h2>
+        <Input type="text" placeholder='E-mail' icon={FiMail} />
+        <Input type="password" placeholder='senha' icon={FiLock} />
+
+        <Button title="Entrar" />
+        <a href="/">Criar conta</a>
+      </Form>
+
+      <Background />
+    </Container>
+  )
+}
