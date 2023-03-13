248c7bcc346128e015f8f64ea3af9a574b019fcb
Adryann Layon <adryann.layon7@gmail.com>
2023-03-13 12:55:40 -0300

    add: Button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.PINK};
+  color: ${({ theme }) => theme.COLORS.BG_TAG};
+  border: none;
+  border-radius: 0.8rem;
+  height: 4.8rem;
+  padding: 1.6rem 3.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+  line-height: 2.1rem;
+  font-feature-settings: 'liga' off;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+`
